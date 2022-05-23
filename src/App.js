7b697c1fbcdc84b015530b2684e79d3405a025a5
luit saikia <luit93@gmail.com>
2022-05-23 22:36:46 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from 'react-bootstrap';
+import { Entry } from './pages/entry/Entry.page';
+
+function App() {
+  return (
+    <div className="App">
+     <Button>m</Button>
+     <Entry/>
+    </div>
+  );
+}
+
+export default App;
