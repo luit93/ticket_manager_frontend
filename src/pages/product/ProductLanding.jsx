2e739a615946a560be26150da998a39db7b0be92
luit saikia <luit93@gmail.com>
2022-06-22 02:03:52 +1000

    fixed img issue on product landing page

--- src/pages/product/ProductLanding.jsx
+++ src/pages/product/ProductLanding.jsx
@@ -1,73 +1,77 @@
-import React from 'react'
-import { Card, Col, Container, ListGroup, Row,Button, Carousel } from 'react-bootstrap'
+import React from "react";
+import {
+  Card,
+  Col,
+  Container,
+  ListGroup,
+  Row,
+  Button,
+  Carousel,
+} from "react-bootstrap";
 import PageBreadcrumb from "../../components/breadcrumb/PageBreadcrumb";
 // import { useState, useEffect } from "react";
 import allProductData from "../../assets/data/product-data.json";
 
-const ProductLanding = ({productName}) => {
-    const productData = allProductData.filter((row) =>
-    row.product.toLowerCase().includes(productName))
+const ProductLanding = ({ productName }) => {
+  const productData = allProductData.filter((row) =>
+    row.product.toLowerCase().includes(productName)
+  );
 
-    console.log(productData)
+  console.log(productData);
 
-    // console.log("data:",productData.product,productData.category,productData.stock)
+  // console.log("data:",productData.product,productData.category,productData.stock)
   return (
     <Container>
-        <Row>
+      <Row>
         <PageBreadcrumb page={productName} />
-        </Row>
-        <Row>
-            <Col>
-            {productData.length?
-            productData.map((row)=>(
-<Card style={{ width: '18rem' }}>
-    
-    <Card.Body>
-      <Card.Title>{row.product}</Card.Title>
-      <Card.Text>
-       {row.desc}
-      </Card.Text>
-    </Card.Body>
-    <ListGroup className="list-group-flush">
-      <ListGroup.Item>Category: {row.category}</ListGroup.Item>
-      <ListGroup.Item>Stock: {row.stock}</ListGroup.Item>
-  
-    </ListGroup>
+      </Row>
 
-    <Card.Body >
-      <Button className='pr-2'>Edit</Button>
-      {" "}
-      <Button className='pl-2'>Delete</Button>
-    </Card.Body>
-  </Card>
-            ))
-        : <h3> Empty</h3>
-        }
-            
-  </Col>
-      
-        </Row>
-        <Row>
+      {productData.length ? (
+        productData.map((row) => (
+            <div>
+          <Row>
             <Col>
-            <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src=""
-          alt="First img"
-        />
-        <Carousel.Caption>
-          <h3>First product slide</h3>
-         
-        </Carousel.Caption>
-      </Carousel.Item>
-    
-    </Carousel>
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>{row.product}</Card.Title>
+                  <Card.Text>{row.desc}</Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item>Category: {row.category}</ListGroup.Item>
+                  <ListGroup.Item>Stock: {row.stock}</ListGroup.Item>
+                </ListGroup>
+
+                <Card.Body>
+                  <Button className="pr-2">Edit</Button>{" "}
+                  <Button className="pl-2">Delete</Button>
+                </Card.Body>
+              </Card>
             </Col>
-        </Row>
+          </Row>
+         
+          <Row>
+           <Col>
+             <Carousel fade>
+               <Carousel.Item>
+                 <img className="d-block w-100" src={row.img} alt="First img" />
+                 <Carousel.Caption>
+                   <h3>{row.desc}</h3>
+                 </Carousel.Caption>
+               </Carousel.Item>
+             </Carousel>
+           </Col>
+         </Row>
+</div>
+
+
+        ))
+      ) : (
+        <h3> Empty</h3>
+      )}
+
+
     </Container>
+  );
+};
 
-  )
-}
-
-export default ProductLanding
+export default ProductLanding;
